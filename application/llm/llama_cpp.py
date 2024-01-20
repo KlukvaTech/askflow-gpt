d9a86aa632999b7abdec2ab00f95fc7d8d2b1780
Foxed1337 <i.z.9191@mail.ru>
2024-01-20 17:25:07 +0500

    Почини промт. Уменьшили передавемы контекст, передаваемый модели

--- application/llm/llama_cpp.py
+++ application/llm/llama_cpp.py
@@ -14,20 +14,26 @@ class LlamaCpp(BaseLLM):
 
     def gen(self, model, engine, messages, stream=False, **kwargs):
         context = messages[0]['content']
+        context = context[0:4000]
         user_question = messages[-1]['content']
-        prompt = f"### Instruction \n {user_question} \n ### Context \n {context} \n ### Answer \n"
-
+        #prompt = f"### Instruction \n {user_question} \n ### Context \n {context} \n ### Answer \n"
+        #prompt = f"[INST] <<SYS>> \n {context} \n <</SYS>> \n {user_question}[/INST] \n"
+        prompt = f"<|im_start|>system \n {context}<|im_end|> \n <|im_start|>user \n {user_question}<|im_end|> \n <|im_start|>assistant\n"
+        print("PROOOOOOOOOOOOOOOOOOOOOOOMT")
+        print(len(prompt))
         result = llama(prompt, max_tokens=150, echo=False)
 
         # import sys
         # print(result['choices'][0]['text'].split('### Answer \n')[-1], file=sys.stderr)
         
-        return result['choices'][0]['text'].split('### Answer \n')[-1]
+        #return result['choices'][0]['text'].split('### Answer \n')[-1]
+        return result['choices'][0]['text'].split('<|im_start|>assistant')[-1]
 
     def gen_stream(self, model, engine, messages, stream=True, **kwargs):
         context = messages[0]['content']
         user_question = messages[-1]['content']
-        prompt = f"### Instruction \n {user_question} \n ### Context \n {context} \n ### Answer \n"
+        #prompt = f"### Instruction \n {user_question} \n ### Context \n {context} \n ### Answer \n"
+        prompt = f"[INST] <<SYS>> \n {context} \n <</SYS>> \n {user_question}[/INST] \n"
 
         result = llama(prompt, max_tokens=150, echo=False, stream=stream)
 
